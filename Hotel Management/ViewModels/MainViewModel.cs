fe993dd2dad295180d09ffe49ae3901edac66f65
ahmed-elhamri <160239005+ahmed-elhamri@users.noreply.github.com>
2024-12-19 00:01:54 +0100

    Adding navigation between views

---
+++ Hotel Management/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Hotel_Management.Commands;
+using Hotel_Management.Views.Admin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Hotel_Management.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public ICommand NavigateCommand { get; }
+
+        public MainViewModel()
+        {
+            NavigateCommand = new RelayCommand(ExecuteNavigateCommand);
+            CurrentView = new DashboardView(); // Default view
+        }
+
+        private void ExecuteNavigateCommand(object viewName)
+        {
+            switch (viewName)
+            {
+                case "DashboardView":
+                    CurrentView = new DashboardView();
+                    break;
+                case "ListEmployesView":
+                    CurrentView = new ListEmployesView();
+                    break;
+                case "ListClientsView":
+                    CurrentView = new ListClientsView();
+                    break;
+                case "ListRoomsView":
+                    CurrentView = new ListRoomsView();
+                    break;
+                case "ListReservationsView":
+                    CurrentView = new ListReservationsView();
+                    break;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Hotel Management/Views/Admin/AdminView.xaml.cs
+++ Hotel Management/Views/Admin/AdminView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hotel_Management.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace Hotel_Management.Views
         public AdminView()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
